37d8f273afa3ba21d212973b362e525631066d9b
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2021-05-02 21:43:36 -0300

    refactor in TP2, "frontera" not implemented

--- TP2/src/tree/Main.java
+++ TP2/src/tree/Main.java
@@ -20,14 +20,53 @@ public class Main {
         add(30);
       }
     };
-    Tree treeNode = new TreeNodeImpl(list);
+    Tree treeNode = new Tree(list);
 
     treeNode.printPreOrder();
-    System.out.println();
     System.out.println("elemento Maximo: " + treeNode.getMaxElem());
     System.out.println("Altura: " + treeNode.getHeight());
     System.out.println("Rama mas larga: " + treeNode.getLongestBranch());
     System.out.println("Elementos al nivel 3: " + treeNode.getElementAtLevel(3));
+    //TODO falta frontera
+    System.out.println("Frontera: " + treeNode.getFrontera());
+    System.out.println();
+
+    treeNode.printPreOrder();
+    System.out.println("añado 23");
+    treeNode.add(23);
+    System.out.println("añado 3");
+    treeNode.add(3);
+    treeNode.printPreOrder();
+    System.out.println("elimino 6");
+    treeNode.delete(6);
+    treeNode.printPreOrder();
+    System.out.println("elimino 30");
+    treeNode.delete(30);
+    treeNode.printPreOrder();
+
+    System.out.println("elemento Maximo: " + treeNode.getMaxElem());
+    System.out.println("Altura: " + treeNode.getHeight());
+    System.out.println("Rama mas larga: " + treeNode.getLongestBranch());
+    System.out.println("Elementos al nivel 2: " + treeNode.getElementAtLevel(2));
+    System.out.println("Frontera: " + treeNode.getFrontera());
+    System.out.println();
+
+    System.out.println("añado 65");
+    treeNode.add(65);
+    treeNode.printPreOrder();
+    System.out.println("elimino 5");
+    treeNode.delete(5);
+    treeNode.printPreOrder();
+    System.out.println("elimino 15");
+    treeNode.delete(15);
+    treeNode.printPreOrder();
+    System.out.println("añado 55");
+    treeNode.add(55);
+    treeNode.printPreOrder();
+    System.out.println("elemento Maximo: "+ treeNode.getMaxElem());
+    System.out.println("Altura: "+treeNode.getHeight());
+    System.out.println("Rama mas larga: "+treeNode.getLongestBranch());
+    System.out.println("Elementos al nivel 2: " +treeNode.getElementAtLevel(2));
     System.out.println("Frontera: " + treeNode.getFrontera());
   }
 
--- TP2/src/tree/Tree.java
+++ TP2/src/tree/Tree.java
@@ -1,54 +1,329 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 
-public abstract class Tree {
+public class Tree {
 
-  public abstract void add(Integer element);
+  private Integer valor;
+  private Tree izquierda;
+  private Tree derecha;
 
-  public abstract Integer getMinimumElementInTree(Tree tree);
+  public Tree(int valor) {
+    this.valor = valor;
+    this.izquierda = null;
+    this.derecha = null;
+  }
 
-  protected abstract void delateLeaf(Tree tree, Tree leaf);
-
-  public abstract Tree getRoot();
-
-  public abstract Tree getLeft();
-
-  public abstract void setLeft(Tree left);
-
-  public abstract Tree getRight();
-
-  public abstract void setRight(Tree right);
-
-  public abstract Integer getValor();
-
-  public abstract void setValue(Integer value);
-
-  //ejecicio 6
-  public abstract boolean hasElem(Integer element);
-
-  public abstract boolean isEmpty();
-
-  public abstract void insert(Integer element);
-
-  public abstract void delete(Integer element);
-
-  public abstract int getHeight();
-
-  public abstract void printPosOrder();
-
-  public abstract void printPreOrder();
-
-  public abstract void printInOrder();
-
-  public abstract List getLongestBranch();
-
-  public abstract List getFrontera();
-
-  public abstract List<Integer> getElementAtLevel(Integer integer);
-
-  public abstract Integer getMaxElem();
+  public Tree(List<Integer> list) {
+    list.forEach(this::add);
+  }
 
 
+  public void add(Integer element) {
+    if (Objects.isNull(valor)) {
+      valor = element;
+      return;
+    }
+    if (element < valor) {
+      if (Objects.isNull(izquierda)) {
+        izquierda = new Tree(element);
+      } else {
+        izquierda.add(element);
+      }
+    } else {
+      if (Objects.isNull(derecha)) {
+        derecha = new Tree(element);
+      } else {
+        derecha.add(element);
+      }
+    }
+  }
+
+
+  public Tree getRoot() {
+    return this;
+  }
+
+  public Tree getLeft() {
+    return izquierda;
+  }
+
+
+  public void setLeft(Tree left) {
+    this.izquierda = left;
+  }
+
+
+  public Tree getRight() {
+    return derecha;
+  }
+
+
+  public void setRight(Tree right) {
+    this.derecha = right;
+  }
+
+
+  public Integer getValor() {
+    return valor;
+  }
+
+
+  public void setValue(Integer value) {
+    this.valor = value;
+  }
+
+
+  public boolean hasElem(Integer element) {
+    if (element.equals(valor)) {
+      return true;
+    } else if (element < valor) {
+      if (Objects.nonNull(izquierda)) {
+        izquierda.hasElem(element);
+      } else {
+        return false;
+      }
+
+    } else {
+      if (Objects.nonNull(derecha)) {
+        derecha.hasElem(element);
+      } else {
+        return false;
+      }
+    }
+    return false;
+  }
+
+
+  public boolean isEmpty() {
+    if (Objects.isNull(valor)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  public void insert(Integer element) {
+    if (element < valor) {
+      if (Objects.nonNull(izquierda)) {
+        izquierda.insert(element);
+      } else {
+        izquierda = new Tree(element);
+      }
+    } else {
+      if (Objects.nonNull(derecha)) {
+        derecha.insert(element);
+      } else {
+        derecha = new Tree(element);
+      }
+    }
+  }
+
+
+  public void delete(Integer element) {
+    delete(this, element);
+  }
+
+  private void delete(Tree tree, Integer integer) {
+
+    if (Objects.nonNull(tree.getLeft()) && integer < tree.getValor()) {
+      if (tree.getLeft().getValor().equals(integer)) {
+        if (Objects.isNull(tree.getLeft().getLeft()) && Objects.isNull(tree.getLeft().getRight())) {
+          tree.setLeft(null);
+        } else if (Objects.nonNull(tree.getLeft().getLeft()) && Objects.isNull(tree.getLeft().getRight())) {
+          tree.setLeft(tree.getLeft().getLeft());
+        } else if (Objects.isNull(tree.getLeft().getLeft()) && Objects.nonNull(tree.getLeft().getRight())) {
+          tree.setLeft(tree.getLeft().getRight());
+        }else if (Objects.nonNull(tree.getLeft().getLeft()) && Objects.nonNull(tree.getLeft().getRight())) {
+          Integer minNodeTreeOfRight = getMinimumElementInTree(tree.getLeft().getRight());
+          delete(tree.getRight(), minNodeTreeOfRight);
+          tree.getLeft().setValue(minNodeTreeOfRight);
+        }
+      } else {
+        tree.getLeft().delete(tree.getLeft(),integer);
+      }
+    } else if (Objects.nonNull(tree.getRight()) && integer > tree.getValor()) {
+      if (tree.getRight().getValor().equals(integer)) {
+        if (Objects.isNull(tree.getRight().getLeft()) && Objects.isNull(tree.getRight().getRight())) {
+          tree.setRight(null);
+        } else if (Objects.nonNull(tree.getRight().getLeft()) && Objects.isNull(tree.getRight().getRight())) {
+          tree.setRight(tree.getRight().getLeft());
+        }
+        else if (Objects.isNull(tree.getRight().getLeft()) && Objects.nonNull(tree.getRight().getRight())) {
+          tree.setLeft(tree.getLeft().getRight());
+        }else if (Objects.nonNull(tree.getRight().getLeft()) && Objects.nonNull(tree.getRight().getRight())) {
+          Integer minNodeTreeOfRight = getMinimumElementInTree(tree.getRight().getRight());
+          delete(tree.getRight(), minNodeTreeOfRight);
+          tree.getRight().setValue(minNodeTreeOfRight);
+        }
+      } else {
+        tree.getLeft().delete(tree.getRight(),integer);
+      }
+    }
+    if (valor.equals(integer)) {
+      if (Objects.nonNull(tree.getLeft()) && Objects.nonNull(tree.getRight())) {
+        Integer minNodeTreeOfRight = getMinimumElementInTree(tree.getRight());
+        delete(tree, minNodeTreeOfRight);
+        tree.setValue(minNodeTreeOfRight);
+      }
+    }
+
+  }
+
+
+  public Integer getMinimumElementInTree(Tree tree) {
+    if (Objects.nonNull(tree.getLeft())) {
+       return tree.getMinimumElementInTree(tree.getLeft());
+    }
+
+    return tree.getValor();
+  }
+
+
+  public int getHeight() {
+    return getHeight(this);
+  }
+
+  private int getHeight(Tree tree) {
+    if (tree == null) {
+      return 0;
+    } else {
+      return (1 + Math.max(getHeight(tree.getLeft()), getHeight(tree.getRight())));
+    }
+
+  }
+
+
+  public void printPosOrder() {
+
+  }
+
+
+  public void printPreOrder() {
+    printPreorder(this);
+    System.out.println();
+  }
+
+  private void printPreorder(Tree node) {
+    if (node == null) {
+      System.out.print("- ");
+      return;
+    }
+
+    System.out.print(node.getValor() + " ");
+
+    printPreorder(node.getLeft());
+
+    /* now recur on right subtree */
+    printPreorder(node.getRight());
+  }
+
+
+  public void printInOrder() {
+
+  }
+
+
+  public List<Integer> getLongestBranch() {
+    List<Integer> result = getLongestBranch(this);
+    Collections.reverse(result);
+    return result;
+  }
+
+  private List<Integer> getLongestBranch(Tree tree) {
+    if (this.valor == null) {
+      List<Integer> output = new ArrayList<>();
+      return output;
+    }
+    List<Integer> left = new ArrayList<>();
+    List<Integer> right = new ArrayList<>();
+    if (Objects.nonNull(tree.getRight())) {
+      right = getLongestBranch(tree.getRight());
+    }
+    if (Objects.nonNull(tree.getLeft())) {
+      left = getLongestBranch(tree.getLeft());
+    }
+
+    if (right.size() < left.size()) {
+      left.add(tree.getValor());
+
+      return left;
+    } else {
+      right.add(tree.getValor());
+
+      return right;
+    }
+  }
+
+
+  public List getFrontera() {
+    List<Integer> frontera = new ArrayList<>();
+    return null;
+  }
+
+  public List getFrontera(Tree tree) {
+    List<Integer> frontera = new ArrayList<>();
+    return null;
+  }
+
+  public List<Integer> getElementAtLevel(Integer integer) {
+    return getElementsLevel(integer, this);
+  }
+
+  private List<Integer> getElementsLevel(Integer level, Tree tree) {
+    List<Integer> list = new ArrayList<>();
+
+    if (tree == null) {
+      return list;
+    }
+
+    if (level == 0) {
+      list.add(tree.getValor());
+    } else if (level > 0) {
+      list.addAll(getElementsLevel(level - 1, tree.getLeft()));
+      list.addAll(getElementsLevel(level - 1, tree.getRight()));
+    }
+
+    return list;
+  }
+
+
+  public Integer getMaxElem() {
+    return getMaxElem(this);
+  }
+
+  private Integer getMaxElem(Tree tree) {
+    if (Objects.isNull(tree.getValor())) {
+      return null;
+    } else {
+      if (Objects.nonNull(tree.getRight())) {
+        return getMaxElem(tree.getRight());
+      } else {
+        return tree.getValor();
+      }
+    }
+  }
+
+
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Tree)) {
+      return false;
+    }
+    Tree tree = (Tree) o;
+    return Objects.equals(getValor(), tree.getValor()) && Objects
+        .equals(izquierda, tree.izquierda) && Objects.equals(derecha, tree.derecha);
+  }
+
+
+  public int hashCode() {
+    return Objects.hash(getValor(), izquierda, derecha);
+  }
 }

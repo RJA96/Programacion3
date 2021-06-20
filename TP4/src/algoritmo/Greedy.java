62312864547f4e7bc90c9cce87abf8297a5df86d
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2021-06-20 14:44:01 -0300

    add utils for load "Empleado" data

--- TP4/src/algoritmo/Greedy.java
+++ TP4/src/algoritmo/Greedy.java
@@ -2,12 +2,19 @@ package algoritmo;
 
 import entidad.Empleado;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Greedy {
 
-    public List<List<Empleado>> resolveGreedy(List<Empleado> empleados, Integer contadorGreedy) {
-        return null;
+    public List<ArrayList<Empleado>> resolveGreedy(ArrayList<Empleado> empleados, Integer contadorGreedy) {
+        ArrayList<ArrayList<Empleado>> resultado = new ArrayList();
+        empleados.sort(Empleado::compareTo);
+        Collections.reverse(empleados);
+        resultado.add(empleados);
+        return resultado;
     }
 
 
--- TP4/src/main/mainGreedy.java
+++ TP4/src/main/mainGreedy.java
@@ -1,7 +1,18 @@
 package main;
 
+import algoritmo.Greedy;
+import entidad.Empleado;
+import utils.EmpleadosUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class mainGreedy {
     public static void main(String[] args) {
+        Greedy greedy = new Greedy();
+        EmpleadosUtils empleadosUtils = new EmpleadosUtils();
+        ArrayList<Empleado> empleados = empleadosUtils.cargarEmpleados();
+        System.out.println(greedy.resolveGreedy(empleados,0));
 
     }
 }
